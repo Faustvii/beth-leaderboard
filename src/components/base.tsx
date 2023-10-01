e65d5edd229d4c2f59ea01610d9ed4627b8a0bea
Faustvii <Faustvii@users.noreply.github.com>
2023-10-01 23:12:10 +0200

    Fixed missing doc type & minify css

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -13,34 +13,27 @@ interface PropsWithSession extends PropsWithChildren {
 }
 
 export const BaseHtml = ({ children, session }: PropsWithSession) => (
-  <html>
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="theme-color" content="#000000" />
-      <title>BETH Leaderboard</title>
-      <script src="https://unpkg.com/htmx.org@1.9.6"></script>
-      {/* <script src="https://unpkg.com/htmx.org/dist/ext/response-targets.js"></script>
-      <script src="https://unpkg.com/htmx.org/dist/ext/loading-states.js"></script>
-      <script src="https://unpkg.com/hyperscript.org@0.9.11"></script> */}
-      <link href="/static/styles.css" rel="stylesheet" />
-      <link rel="icon" href="data:;base64,="></link>
-      {/* <style>
-        {`
-          [data-loading] {
-            display: none;
-          }
-        `}
-      </style> */}
-      <script>{safeScript}</script>
-    </head>
-    <body
-      hx-boost="true"
-      class="h-screen w-full place-items-center bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100"
-    >
-      <LoadingBarHtml />
-      <NavbarHtml session={session} />
-      {children}
-    </body>
-  </html>
+  <>
+    {"<!DOCTYPE html>"}
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#000000" />
+        <title>BETH Leaderboard</title>
+        <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+        <link href="/static/styles.css" rel="stylesheet" />
+        <link rel="icon" href="data:;base64,="></link>
+        <script>{safeScript}</script>
+      </head>
+      <body
+        hx-boost="true"
+        class="h-screen w-full place-items-center bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100"
+      >
+        <LoadingBarHtml />
+        <NavbarHtml session={session} />
+        {children}
+      </body>
+    </html>
+  </>
 );

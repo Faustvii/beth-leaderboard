69bc1e05ef5ad892bdd12329f731182d84412b8a
Faustvii <Faustvii@users.noreply.github.com>
2023-10-08 10:56:35 +0200

    Made signin button be a icon

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,8 +7,8 @@ export const AnchorButtonHtml = ({
   <>
     <a
       hx-indicator=".progress-bar"
-      {...props}
       class="mx-1 rounded-lg bg-blue-500 px-2 py-2 text-white transition duration-200 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+      {...props}
     >
       {children}
     </a>

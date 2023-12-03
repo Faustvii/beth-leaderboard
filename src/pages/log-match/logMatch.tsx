f4022214149232a8c2897f8b60e239a539bd52e9
Kechr <55458479+Kechr@users.noreply.github.com>
2023-12-03 13:28:46 +0100

    Make use of clsx consistent

--- src/pages/log-match/logMatch.tsx
+++ src/pages/log-match/logMatch.tsx
@@ -179,14 +179,10 @@ function matchSearchResults(results: { name: string; id: string }[]) {
       {results.map((result) => (
         <button
           id={result.id}
-          class={clsx(
-            [
-              "w-full p-3 pl-10 text-left hover:bg-primary/50 last:hover:rounded-b-lg",
-            ],
-            [
-              "focus-visible:outline-none focus-visible:ring focus-visible:ring-primary/50 last:focus-visible:rounded-b-lg",
-            ],
-          )}
+          class={clsx([
+            "w-full p-3 pl-10 text-left hover:bg-primary/50 last:hover:rounded-b-lg",
+            "focus-visible:outline-none focus-visible:ring focus-visible:ring-primary/50 last:focus-visible:rounded-b-lg",
+          ])}
           value={result.name}
           // _={`on click halt the event then put my id into #white1Id@value then set #white1-input's value to my value then add @hidden to #white1-results`}
           _="on click halt the event then add @hidden to the closest <div/> then put my value into the value of the previous <input/> from me then put my id into the value of the next <input/>"

c6f920561136425245bc7404d65f8ac4c19a9456
Faustvii <Faustvii@users.noreply.github.com>
2024-01-15 17:20:12 +0100

    Automatic seeding of preprod

---
+++ src/db/preprod.tsx
@@ -0,0 +1,143 @@
+import { generateRandomString } from "lucia/utils";
+import { type readDb } from ".";
+import { config } from "../config";
+import { applyMatchResult, matchEloChange } from "../lib/elo";
+import { type GameResult } from "../types/elo";
+import { playerEloQuery } from "./queries/matchQueries";
+import { getActiveSeason } from "./queries/seasonQueries";
+import { matches, seasonsTbl, userTbl } from "./schema";
+import { type InsertSeason } from "./schema/season";
+
+export async function SeedPreprod(db: typeof readDb) {
+  if (config.env.NODE_ENV === "preprod") {
+    console.log("starting database seeding");
+    const activeSeason = await SeedSeason(db);
+    if (!activeSeason) throw new Error("Wasn't able to seed an active season");
+    await SeedPlayers(db);
+    await SeedMatches(db, activeSeason);
+    console.log("database seeding complete");
+  }
+}
+
+async function SeedSeason(db: typeof readDb) {
+  const activeSeason = await getActiveSeason();
+  if (!activeSeason) {
+    const now = new Date();
+    const startAt = new Date(now.getFullYear(), 0, 1);
+    const endAt = new Date(now.getFullYear() + 1, 0, 1);
+    const season: InsertSeason = {
+      name: `Season ${now.getFullYear()}`,
+      startAt: startAt,
+      endAt: endAt,
+    };
+    console.log("Creating season", season);
+    await db.insert(seasonsTbl).values(season);
+    return await getActiveSeason();
+  }
+  return activeSeason;
+}
+
+async function SeedPlayers(db: typeof readDb) {
+  for (let index = 0; index < 100; index++) {
+    const player: typeof userTbl.$inferInsert = {
+      id: generateRandomString(15),
+      name: `Player ${index}`,
+      elo: 1500,
+      email: "fake@fake.crokinole",
+    };
+
+    await db.insert(userTbl).values(player);
+  }
+}
+
+async function SeedMatches(
+  db: typeof readDb,
+  activeSeason: typeof seasonsTbl.$inferSelect,
+) {
+  const startOfYear = new Date(new Date().getFullYear(), 0, 1).getTime();
+  const endOfYear = new Date(new Date().getFullYear() + 1, 0, 1).getTime() - 1;
+  for (let index = 0; index < 1000; index++) {
+    const players = await db.query.userTbl.findMany({
+      columns: {
+        id: true,
+      },
+    });
+    if (index % 50 == 0)
+      console.log("creating elo match " + index + " of 1000");
+
+    const userIds = [
+      players[Math.floor(Math.random() * players.length)].id,
+      players[Math.floor(Math.random() * players.length)].id,
+      players[Math.floor(Math.random() * players.length)].id,
+      players[Math.floor(Math.random() * players.length)].id,
+    ];
+
+    const eloPlayers = userIds.map(async (id) => ({
+      id: id,
+      elo: await playerEloQuery(id, activeSeason?.id ?? 1),
+    }));
+    const whitePlayerOne = await eloPlayers[0];
+    const whitePlayerTwo = await eloPlayers[1];
+    const blackPlayerOne = await eloPlayers[2];
+    const blackPlayerTwo = await eloPlayers[3];
+    // Generate a random date within this year
+    const matchDate = new Date(
+      startOfYear + Math.random() * (endOfYear - startOfYear),
+    );
+
+    const result =
+      Math.random() > 0.9 ? "Draw" : Math.random() > 0.5 ? "Black" : "White";
+    //960 is 4 rounds of only 20 shots and all misses for opposite team
+    const scoreDiff =
+      result === "Draw" ? 0 : Math.min(Math.floor(Math.random() * 20) * 5, 960);
+    const match: GameResult = {
+      outcome:
+        result === "Black" ? "loss" : result === "White" ? "win" : "draw",
+      teams: [
+        {
+          color: "White",
+          players: [
+            {
+              id: whitePlayerOne.id,
+              elo: whitePlayerOne.elo,
+            },
+            {
+              id: whitePlayerTwo.id,
+              elo: whitePlayerTwo.elo,
+            },
+          ],
+        },
+        {
+          color: "Black",
+          players: [
+            {
+              id: blackPlayerOne.id,
+              elo: blackPlayerOne.elo,
+            },
+            {
+              id: blackPlayerTwo.id,
+              elo: blackPlayerTwo.elo,
+            },
+          ],
+        },
+      ],
+    };
+    const eloChange = matchEloChange(match);
+    applyMatchResult({ eloFloor: 0 }, match);
+
+    const matchInsert: typeof matches.$inferInsert = {
+      whitePlayerOne: whitePlayerOne.id,
+      blackPlayerOne: blackPlayerOne.id,
+      result: result,
+      scoreDiff: scoreDiff,
+      whiteEloChange: eloChange.white,
+      blackEloChange: eloChange.black,
+      whitePlayerTwo: whitePlayerTwo.id,
+      blackPlayerTwo: blackPlayerTwo.id,
+      createdAt: matchDate,
+      seasonId: activeSeason?.id ?? 1,
+    };
+
+    await db.insert(matches).values(matchInsert);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import "@kitajs/html/register";
-import { readableStreamToText } from "bun";
 import { migrate } from "drizzle-orm/libsql/migrator";
 import { Elysia } from "elysia";
 import { config } from "./config";
 import { api } from "./controllers/*";
 import { writeDb } from "./db";
+import { SeedPreprod } from "./db/preprod";
 import { pages } from "./pages/*";
 import { staticController } from "./staticFiles";
 import { webSockets } from "./websockets/*";
@@ -12,14 +12,6 @@ import { webSockets } from "./websockets/*";
 console.log("migrating database");
 await migrate(writeDb, { migrationsFolder: "./drizzle" });
 console.log("database migrated");
-
-if (config.env.NODE_ENV === "preprod") {
-  console.log("starting database seeding");
-  const { stdout } = Bun.spawn(["bun", "./src/db/seed.ts"]);
-  const text = await readableStreamToText(stdout);
-  console.log(text);
-  console.log("database seeding complete");
-}
 
 const app = new Elysia()
   .use(webSockets)
@@ -35,3 +27,7 @@ export type App = typeof app;
 console.log(
   `app is listening on http://${app.server?.hostname}:${app.server?.port}`,
 );
+
+if (config.env.NODE_ENV === "preprod") {
+  await SeedPreprod(writeDb);
+}

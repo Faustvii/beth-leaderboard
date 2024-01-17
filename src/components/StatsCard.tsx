4a06236d50dff8b83907e6cd1074ab9d901b7e90
srensommer <srensommer@gmail.com>
2024-01-17 14:06:57 +0100

    wowee PR comments fixed

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -3,10 +3,16 @@ import { cn } from "../lib/utils";
 interface Props {
   children: JSX.Element;
   title: string;
+  doubleSize?: boolean;
 }
 
-export const StatsCardHtml = ({ title, children }: Props) => (
-  <div class={cn("col-span-6 flex h-full flex-col rounded-lg p-5 text-white")}>
+export const StatsCardHtml = ({ title, children, doubleSize }: Props) => (
+  <div
+    class={cn(
+      doubleSize ? "col-span-6 md:col-span-12" : "col-span-6",
+      "flex h-full flex-col rounded-lg p-5 text-white",
+    )}
+  >
     <h2 class="border-b-[1px] border-primary/60 p-2 text-2xl font-semibold">
       {title}
     </h2>

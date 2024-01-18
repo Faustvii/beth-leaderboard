d07f8daba2af4aa085a4643e5d8ba1aa66b30fff
Faustvii <Faustvii@users.noreply.github.com>
2024-01-18 20:55:06 +0100

    Changed some js <script> to be async

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -11,10 +11,19 @@ export const BaseHtml = ({ children }: PropsWithChildren) => (
         <meta name="theme-color" content="#000000" />
         <title>BETH Leaderboard</title>
         <script src="https://unpkg.com/htmx.org@1.9.6"></script>
-        <script src="https://unpkg.com/htmx.org@1.9.6/dist/ext/ws.js"></script>
-        <script src="https://unpkg.com/htmx.org@1.9.6/dist/ext/response-targets.js"></script>
         <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script
+          src="https://unpkg.com/htmx.org@1.9.6/dist/ext/ws.js"
+          async
+        ></script>
+        <script
+          src="https://unpkg.com/htmx.org@1.9.6/dist/ext/response-targets.js"
+          async
+        ></script>
+        <script
+          src="https://unpkg.com/chart.js@4.4.1/dist/chart.umd.js"
+          async
+        ></script>
         <link href="/public/styles.css" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -22,7 +31,6 @@ export const BaseHtml = ({ children }: PropsWithChildren) => (
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
-
         <link rel="icon" href="/public/favicon-16x16.png" sizes="16x16" />
         <link rel="icon" href="/public/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" href="/public/favicon-192x192.png" sizes="192x192" />

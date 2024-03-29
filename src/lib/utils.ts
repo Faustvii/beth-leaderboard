0676684995d6d64bedaaf8ce137a48bc5d624e61
Jonas Nielsen <djonnneller@gmail.com>
2024-03-29 18:53:06 +0100

    First draft for generic scoring system

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,7 @@ import clsx, { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
+
+export const isDefined = <T>(x: T | null | undefined): x is T => {
+  return typeof x !== "undefined" && x !== null;
+};

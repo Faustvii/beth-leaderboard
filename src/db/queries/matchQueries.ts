4af2bd16fd0823635b68ff36a60348b0e47f76ed
Jonas Nielsen <djonnneller@gmail.com>
2024-04-04 19:02:43 +0200

    Rename scoring to rating

--- src/db/queries/matchQueries.ts
+++ src/db/queries/matchQueries.ts
@@ -2,7 +2,7 @@ import { and, desc, eq, inArray, isNotNull, or, sql, sum } from "drizzle-orm";
 import { unionAll } from "drizzle-orm/sqlite-core";
 import { readDb } from "..";
 import { notEmpty, unique } from "../../lib";
-import { elo, getScores, Match } from "../../lib/scoring";
+import { elo, getRatings, Match } from "../../lib/rating";
 import { matches, userTbl } from "../schema";
 
 export const getMatches = async (seasonId: number): Promise<Match[]> => {
@@ -191,37 +191,37 @@ export const playerEloQuery = async (userId: string, seasonId: number) => {
 
 export const playersEloQuery = async (userIds: string[], seasonId: number) => {
   const matches = await getMatches(seasonId);
-  const eloScoring = elo();
-  const eloScores = getScores(matches, eloScoring)
+  const eloRatingSystem = elo();
+  const eloRatings = getRatings(matches, eloRatingSystem)
     .filter((x) => userIds.includes(x.player.id))
     .map((x) => ({
       id: x.player.id,
-      elo: x.score,
+      elo: x.rating,
     }));
 
   // None of the players have played any matches
-  if (eloScores.length === 0) {
+  if (eloRatings.length === 0) {
     return userIds.map((id) => ({
       id,
       elo: 1500,
     }));
   }
   // Some of the players haven't played any matches
-  if (eloScores.length !== userIds.length) {
+  if (eloRatings.length !== userIds.length) {
     const missingIds = userIds.filter(
-      (id) => !eloScores.find((elo) => elo.id === id),
+      (id) => !eloRatings.find((elo) => elo.id === id),
     );
-    eloScores.push(
+    eloRatings.push(
       ...missingIds.map((id) => ({
         id: id,
-        elo: eloScoring.defaultScore,
+        elo: eloRatingSystem.defaultRating,
       })),
     );
   }
 
-  return eloScores.map((elo) => ({
+  return eloRatings.map((elo) => ({
     id: elo.id,
-    elo: elo.elo ?? eloScoring.defaultScore,
+    elo: elo.elo ?? eloRatingSystem.defaultRating,
   }));
 };
 
@@ -232,15 +232,15 @@ export const playerEloPaginationQuery = async (
   const pageSize = 15;
 
   const matches = await getMatches(seasonId);
-  const eloScoring = elo();
-  const eloScores = getScores(matches, eloScoring);
+  const eloRatingSystem = elo();
+  const eloRatings = getRatings(matches, eloRatingSystem);
 
   const startIndex = (page - 1) * pageSize;
   const endIndex = startIndex + pageSize - 1;
 
-  return eloScores.slice(startIndex, endIndex).map((x) => ({
+  return eloRatings.slice(startIndex, endIndex).map((x) => ({
     id: x.player.id,
     name: x.player.name,
-    elo: x.score,
+    elo: x.rating,
   }));
 };

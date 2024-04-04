51091ec82df3f0d037bdfea178b9988b7d258221
Jonas Nielsen <djonnneller@gmail.com>
2024-04-04 19:26:33 +0200

    Remove more unused functions

--- src/db/queries/matchQueries.ts
+++ src/db/queries/matchQueries.ts
@@ -1,8 +1,7 @@
-import { and, desc, eq, inArray, isNotNull, or, sql, sum } from "drizzle-orm";
-import { unionAll } from "drizzle-orm/sqlite-core";
+import { and, eq, inArray, or } from "drizzle-orm";
 import { readDb } from "..";
 import { notEmpty, unique } from "../../lib";
-import { elo, getRatings, Match } from "../../lib/rating";
+import { elo, getRatings, type Match } from "../../lib/rating";
 import { matches, userTbl } from "../schema";
 
 export const getMatches = async (seasonId: number): Promise<Match[]> => {
@@ -113,42 +112,6 @@ export const getMatchesWithPlayers = async (
       whitePlayerTwo,
     };
   });
-};
-
-export const playersEloQuery = async (userIds: string[], seasonId: number) => {
-  const matches = await getMatches(seasonId);
-  const eloRatingSystem = elo();
-  const eloRatings = getRatings(matches, eloRatingSystem)
-    .filter((x) => userIds.includes(x.player.id))
-    .map((x) => ({
-      id: x.player.id,
-      elo: x.rating,
-    }));
-
-  // None of the players have played any matches
-  if (eloRatings.length === 0) {
-    return userIds.map((id) => ({
-      id,
-      elo: 1500,
-    }));
-  }
-  // Some of the players haven't played any matches
-  if (eloRatings.length !== userIds.length) {
-    const missingIds = userIds.filter(
-      (id) => !eloRatings.find((elo) => elo.id === id),
-    );
-    eloRatings.push(
-      ...missingIds.map((id) => ({
-        id: id,
-        elo: eloRatingSystem.defaultRating,
-      })),
-    );
-  }
-
-  return eloRatings.map((elo) => ({
-    id: elo.id,
-    elo: elo.elo ?? eloRatingSystem.defaultRating,
-  }));
 };
 
 export const playerEloPaginationQuery = async (

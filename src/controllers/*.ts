a3033bf74800057a2d82d748afc765f5ebfcdd2e
Jonas Nielsen <djonnneller@gmail.com>
2024-04-04 18:39:16 +0200

    Remove debug endpoint

--- src/controllers/*.ts
+++ src/controllers/*.ts
@@ -1,11 +1,9 @@
 import Elysia from "elysia";
 import { authController } from "./auth";
 import { imageGen } from "./imageGeneration";
-import { scores } from "./scores";
 
 export const api = new Elysia({
   prefix: "/api",
 })
   .use(authController)
-  .use(imageGen)
-  .use(scores); // TODO: Remove scores from here
+  .use(imageGen);

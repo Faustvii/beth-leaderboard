987cb0a2f05dbeafdd8ed8e1e73c4547da80fb0d
Jonas Nielsen <djonnneller@gmail.com>
2024-05-29 10:18:46 +0200

    Fix seeded games having same player multiple times

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,26 @@ export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
 export const isDefined = <T>(x: T | null | undefined): x is T => {
   return typeof x !== "undefined" && x !== null;
 };
+
+/**
+ * @param array Array to pick from
+ * @param count Count of elements to pick
+ * @returns Array of length count containing distinct elements from input array
+ */
+export function pick<T>(array: T[], count: number) {
+  const result: T[] = [];
+
+  const nonPickedIndices = Array(array.length)
+    .fill(undefined)
+    .map((_, i) => i);
+
+  for (let i = 0; i < count; i++) {
+    const randomIndexIndex = Math.floor(
+      Math.random() * nonPickedIndices.length,
+    );
+    const pickedIndex = nonPickedIndices.splice(randomIndexIndex, 1)[0];
+    result.push(array[pickedIndex]);
+  }
+
+  return result;
+}

553f0ca33b1a841ea2377dd235a6f271f852420a
Kechr <55458479+Kechr@users.noreply.github.com>
2024-06-19 13:51:44 +0200

    Make existing seasons responsive

--- src/pages/admin/components/ExisitngSeasons.tsx
+++ src/pages/admin/components/ExisitngSeasons.tsx
@@ -9,16 +9,18 @@ interface ExistingSeasonsProps {
 export const ExistingSeasons = ({ seasons }: ExistingSeasonsProps) => {
   return (
     <div class="mt-3 flex flex-col gap-3">
-      <div class="flex gap-3 px-4 font-semibold ">
+      <div class="hidden gap-3 px-4 font-semibold md:flex">
         <p class="w-[10%]">Name</p>
         <p class="w-1/4">Start</p>
         <p class="w-1/4">End</p>
         <p class="w-1/4">Rating System</p>
       </div>
       {seasons.map((season) => (
-        <div class="flex items-center gap-3 rounded-lg border px-4 py-2">
-          <p class="w-[10%]">{season.name}</p>
-          <p class="w-1/4 truncate">
+        <div class="flex flex-col items-center rounded-lg border px-4 py-2 md:flex-row md:gap-3">
+          <p class="w-full pt-3 font-semibold md:hidden">Name</p>
+          <p class="w-full truncate md:w-[10%]">{season.name}</p>
+          <p class="w-full pt-3 font-semibold md:hidden">Start</p>
+          <p class="w-full truncate md:w-1/4">
             {season.startAt.toLocaleString("en-US", {
               hourCycle: "h24",
               year: "numeric",
@@ -28,7 +30,8 @@ export const ExistingSeasons = ({ seasons }: ExistingSeasonsProps) => {
               minute: "2-digit",
             })}
           </p>
-          <p class="w-1/4 truncate">
+          <p class="w-full pt-3 font-semibold md:hidden">End</p>
+          <p class="w-full truncate md:w-1/4">
             {season.endAt.toLocaleString("en-US", {
               hourCycle: "h24",
               year: "numeric",
@@ -38,26 +41,34 @@ export const ExistingSeasons = ({ seasons }: ExistingSeasonsProps) => {
               minute: "2-digit",
             })}
           </p>
-          <p>{season.ratingSystem.toUpperCase()}</p>
+          <p class="w-full pt-3 font-semibold md:hidden">Rating System</p>
+          <p class="w-full truncate md:w-[15%]">
+            {season.ratingSystem.toUpperCase()}
+          </p>
 
-          <div class={cn(" ml-auto flex")}>
+          <div
+            class={cn("mb-2 ml-auto mt-4 flex w-full md:mb-0 md:mt-0 md:w-24")}
+          >
             <button
               hx-get={`admin/season/${season.id}`}
               hx-target="#mainContainer"
               hx-swap="afterend"
               class={cn(
-                "flex w-1/2 justify-center gap-3 rounded-l-lg",
-                "p-2 hover:text-teal-600",
+                "flex w-1/2 justify-center gap-3 rounded-l-lg p-2",
+                "md:bg-transparent md:hover:bg-transparent md:hover:text-teal-600",
+                "bg-teal-700 hover:bg-teal-700/85 hover:text-gray-300",
               )}
               _={`on htmx:afterSettle js htmx.process(document.body) end`}
             >
               <EditIcon />
+              <p class="md:hidden">Edit</p>
             </button>
             <button
               type="button"
               class={cn(
-                "flex w-1/2 justify-center gap-3 rounded-r-lg",
-                "p-2 hover:text-red-600 disabled:bg-gray-600",
+                "flex w-1/2 justify-center gap-3 rounded-r-lg p-2",
+                "bg-red-700 hover:bg-red-700/85 hover:text-gray-300",
+                "md:bg-transparent md:hover:bg-transparent md:hover:text-red-600 md:disabled:text-gray-600",
               )}
               hx-indicator=".progress-bar"
               hx-target="#mainContainer"
@@ -67,6 +78,7 @@ export const ExistingSeasons = ({ seasons }: ExistingSeasonsProps) => {
               _="on htmx:beforeRequest set innerText of <p/> in me to 'Deleting...'"
             >
               <TrashIcon />
+              <p class="md:hidden">Delete</p>
             </button>
           </div>
         </div>

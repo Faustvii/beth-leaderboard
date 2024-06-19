6a2fa018120b220f1de0b04dee3be41010925c0a
Kechr <55458479+Kechr@users.noreply.github.com>
2024-06-19 11:24:28 +0200

    Add edit season func

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -15,9 +15,11 @@ import {
 import {
   deleteSeason,
   getActiveSeason,
+  getSeason,
   getSeasons,
 } from "../../db/queries/seasonQueries";
 import { matches, seasonsTbl } from "../../db/schema";
+import { type Season } from "../../db/schema/season";
 import { isHxRequest, redirect } from "../../lib";
 import { syncIfLocal } from "../../lib/dbHelpers";
 import { EditIcon, TrashIcon } from "../../lib/icons";
@@ -57,8 +59,15 @@ export const Admin = new Elysia({
 
     return <EditMatchModal match={matchToEdit} />;
   })
+  .get("/season/:id", async ({ params: { id } }) => {
+    const seasonToEdit = await getSeason(Number(id));
+
+    if (!seasonToEdit) return;
+
+    return <EditSeasonModal season={seasonToEdit} />;
+  })
   .post(
-    "/",
+    "/match",
     async ({ set, headers, body, writeDb }) => {
       await writeDb
         .update(matches)
@@ -136,13 +145,38 @@ export const Admin = new Elysia({
     },
   )
   .post(
+    "/season",
+    async ({ set, headers, body, writeDb }) => {
+      await writeDb
+        .update(seasonsTbl)
+        .set({
+          name: body.seasonName,
+          startAt: new Date(body.seasonStart),
+          endAt: new Date(body.seasonEnd),
+          ratingSystem: body.ratingSystem,
+        })
+        .where(eq(seasonsTbl.id, Number(body.seasonId)));
+
+      redirect({ headers, set }, `/admin`);
+    },
+    {
+      body: t.Object({
+        seasonId: t.String(),
+        seasonName: t.String({ minLength: 1 }),
+        seasonStart: t.String({ minLength: 1 }),
+        seasonEnd: t.String({ minLength: 1 }),
+        ratingSystem: t.Enum({ elo: "elo", openskill: "openskill" }),
+      }),
+    },
+  )
+  .post(
     "/new-season",
     async ({ set, headers, body, writeDb }) => {
       type newSeason = typeof seasonsTbl.$inferInsert;
       const seasonToInsert: newSeason = {
-        name: body.newSeasonName,
-        startAt: new Date(body.newSeasonStart),
-        endAt: new Date(body.newSeasonEnd),
+        name: body.seasonName,
+        startAt: new Date(body.seasonStart),
+        endAt: new Date(body.seasonEnd),
         ratingSystem: body.ratingSystem,
       };
 
@@ -153,8 +187,8 @@ export const Admin = new Elysia({
     },
     {
       beforeHandle: ({ body }) => {
-        const startDate = new Date(body.newSeasonStart);
-        const endDate = new Date(body.newSeasonEnd);
+        const startDate = new Date(body.seasonStart);
+        const endDate = new Date(body.seasonEnd);
         const now = new Date(Date.now());
 
         if (startDate > endDate) {
@@ -176,10 +210,10 @@ export const Admin = new Elysia({
         return;
       },
       body: t.Object({
-        newSeasonStart: t.String({ minLength: 1 }),
-        newSeasonEnd: t.String({ minLength: 1 }),
+        seasonName: t.String({ minLength: 1 }),
+        seasonStart: t.String({ minLength: 1 }),
+        seasonEnd: t.String({ minLength: 1 }),
         ratingSystem: t.Enum({ elo: "elo", openskill: "openskill" }),
-        newSeasonName: t.String({ minLength: 1 }),
       }),
     },
   );
@@ -215,58 +249,20 @@ async function page(session: Session | null) {
       <HeaderHtml title="With great power comes great responsibility" />
       <StatsCardHtml title="Seasons" doubleSize>
         <div class="flex flex-col">
-          <form
-            id="newSeasonForm"
-            method="post"
-            enctype="multipart/form-data"
-            hx-ext="response-targets"
-            hx-target-400="#errors"
-          >
-            <label for="newSeasonStart">Start</label>
-            <input
-              id="newSeasonStart"
-              name="newSeasonStart"
-              form="newSeasonForm"
-              class="text-black"
-              type="datetime-local"
-            />
-            <label for="newSeasonEnd">End</label>
-            <input
-              id="newSeasonEnd"
-              name="newSeasonEnd"
-              form="newSeasonForm"
-              class="text-black"
-              type="datetime-local"
-            />
-            <label for="ratingSystemSelect">Rating system:</label>
-            <select
-              id="ratingSystemSelect"
-              name="ratingSystem"
-              form="newSeasonForm"
-              class="text-black"
-            >
-              <option value="openskill">Openskill</option>
-              <option value="elo">ELO</option>
-            </select>
-            <label for="newSeasonName">Season name:</label>
-            <input
-              id="newSeasonName"
-              name="newSeasonName"
-              form="newSeasonForm"
-              class="text-black"
-              type="text"
-              value={`Season ${(seasons.length + 1).toString()}`}
-            />
-            <button
-              hx-post="/admin/new-season"
-              hx-indicator=".progress-bar"
-              class="rounded-lg bg-teal-700 p-2"
-              type="button"
-            >
-              Create new Season
-            </button>
-            <div id="errors" class="text-red-500"></div>
-          </form>
+          <SeasonForm
+            formId="newSeason"
+            actionButtons={
+              <button
+                hx-post="/admin/new-season"
+                hx-indicator=".progress-bar"
+                class="rounded-lg bg-teal-700 p-2"
+                type="button"
+              >
+                Create new Season
+              </button>
+            }
+            amountOfSeasons={seasons.length}
+          />
           <div>
             {seasons.map((season) => (
               <div>
@@ -292,27 +288,37 @@ async function page(session: Session | null) {
                   })}
                 </p>
                 <p>{season.ratingSystem.toUpperCase()}</p>
-                <button
-                  class={cn(
-                    "mt-2 flex w-1/2 justify-center gap-3 rounded-lg bg-teal-700 p-2 hover:bg-teal-700/85",
-                  )}
-                >
-                  <EditIcon />
-                  <p class="hidden sm:block">Edit</p>
-                </button>
-                <button
-                  type="button"
-                  class="mt-2 flex w-1/2 justify-center gap-3 rounded-lg bg-red-700 p-2 hover:bg-red-700/85"
-                  hx-indicator=".progress-bar"
-                  hx-target="#mainContainer"
-                  hx-delete={`admin/season/${season.id}`}
-                  hx-disabled-elt="this"
-                  hx-confirm="Are you sure you wish to delete this season? This will result in all matches in this season being deleted as well."
-                  _="on htmx:beforeRequest set innerText of <p/> in me to 'Deleting...'"
-                >
-                  <TrashIcon />
-                  <p class="hidden sm:block">Delete</p>
-                </button>
+                <div class={cn("mt-auto flex")}>
+                  <button
+                    hx-get={`admin/season/${season.id}`}
+                    hx-target="#mainContainer"
+                    hx-swap="afterend"
+                    class={cn(
+                      "mt-2 flex w-1/2 justify-center gap-3 rounded-l-lg",
+                      "bg-teal-700 p-2 hover:bg-teal-700/85",
+                    )}
+                    _={`on htmx:afterSettle js htmx.process(document.body) end`}
+                  >
+                    <EditIcon />
+                    <p class="hidden sm:block">Edit</p>
+                  </button>
+                  <button
+                    type="button"
+                    class={cn(
+                      "mt-2 flex w-1/2 justify-center gap-3 rounded-r-lg",
+                      "bg-red-700 p-2 hover:bg-red-700/85 disabled:bg-gray-600",
+                    )}
+                    hx-indicator=".progress-bar"
+                    hx-target="#mainContainer"
+                    hx-delete={`admin/season/${season.id}`}
+                    hx-disabled-elt="this"
+                    hx-confirm="Are you sure you wish to delete this season?"
+                    _="on htmx:beforeRequest set innerText of <p/> in me to 'Deleting...'"
+                  >
+                    <TrashIcon />
+                    <p class="hidden sm:block">Delete</p>
+                  </button>
+                </div>
               </div>
             ))}
           </div>
@@ -365,7 +371,7 @@ const EditMatchModal = ({ match }: EditMatchModalProps) => {
                 Cancel
               </button>
               <button
-                hx-post="/admin"
+                hx-post="/admin/match"
                 type="submit"
                 class="rounded-lg bg-teal-700 p-2"
                 hx-indicator=".progress-bar"
@@ -378,5 +384,138 @@ const EditMatchModal = ({ match }: EditMatchModalProps) => {
         />
       </div>
     </div>
+  );
+};
+
+interface EditSeasonModalProps {
+  season: Season;
+}
+
+const EditSeasonModal = ({ season }: EditSeasonModalProps) => {
+  return (
+    <div
+      id="edit-season-modal"
+      class={cn(
+        "fixed bottom-0 left-0 right-0 top-0 z-40 backdrop-brightness-50",
+        "flex flex-col items-center justify-center",
+        // TODO: Add animation
+      )}
+      _="on closeEditModal remove me"
+    >
+      <div
+        class="absolute bottom-0 left-0 right-0 top-0 -z-50"
+        _="on click trigger closeEditModal"
+      />
+      <div class="-z-20 w-[80%] max-w-[600px] rounded-md bg-slate-800 p-4 text-white shadow-md lg:p-8">
+        <h1 class="mb-4 text-2xl font-semibold">Edit season</h1>
+
+        <SeasonForm
+          formId={`edit-season-${season.id}-form`}
+          season={season}
+          actionButtons={
+            <div class="mt-3 flex justify-end gap-3">
+              <button
+                type="button"
+                class="rounded-lg bg-red-700 p-2"
+                _="on click trigger closeEditModal"
+              >
+                Cancel
+              </button>
+              <button
+                hx-post="/admin/season"
+                type="submit"
+                class="rounded-lg bg-teal-700 p-2"
+                hx-indicator=".progress-bar"
+                _="on click set my.innerText to 'Saving...' then wait for htmx:afterRequest then set my.innerText to 'Save'"
+              >
+                Save
+              </button>
+            </div>
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+interface SeasonFormProps {
+  formId: string;
+  actionButtons: JSX.Element;
+  amountOfSeasons?: number;
+  season?: Season;
+}
+
+const SeasonForm = ({
+  formId,
+  actionButtons,
+  amountOfSeasons,
+  season,
+}: SeasonFormProps) => {
+  let startAt = "";
+  let endAt = "";
+
+  // substring 0 - 16 to remove milliseconds and timezone
+  if (season?.startAt) startAt = season.startAt.toISOString().substring(0, 16);
+  if (season?.endAt) endAt = season.endAt.toISOString().substring(0, 16);
+
+  return (
+    <form
+      id={formId}
+      method="post"
+      enctype="multipart/form-data"
+      hx-ext="response-targets"
+      hx-target-400="#errors"
+    >
+      <label for="seasonStart">Start</label>
+      <input
+        id="seasonStart"
+        name="seasonStart"
+        form={formId}
+        class="text-black"
+        type="datetime-local"
+        value={startAt}
+      />
+      <label for="seasonEnd">End</label>
+      <input
+        id="seasonEnd"
+        name="seasonEnd"
+        form={formId}
+        class="text-black"
+        type="datetime-local"
+        value={endAt}
+      />
+      <label for="ratingSystemSelect">Rating system:</label>
+      <select
+        id="ratingSystemSelect"
+        name="ratingSystem"
+        form={formId}
+        class="text-black"
+      >
+        <option
+          value="openskill"
+          selected={season?.ratingSystem === "openskill"}
+        >
+          Openskill
+        </option>
+        <option value="elo" selected={season?.ratingSystem === "elo"}>
+          ELO
+        </option>
+      </select>
+      <label for="seasonName">Season name:</label>
+      <input
+        id="seasonName"
+        name="seasonName"
+        form={formId}
+        class="text-black"
+        type="text"
+        value={
+          season?.name ??
+          `Season ${amountOfSeasons ? amountOfSeasons + 1 : "name"}`
+        }
+      />
+      {actionButtons}
+      <div id="errors" class="text-red-500"></div>
+      {season && <input hidden name="seasonId" value={season.id.toString()} />}
+    </form>
   );
 };

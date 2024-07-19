21eee0d1586286d4d552c3b6b77dc6f61c01276d
Kechr <55458479+Kechr@users.noreply.github.com>
2024-07-19 13:43:45 +0200

    Make date and time picker a seperate component

--- src/components/MatchForm.tsx
+++ src/components/MatchForm.tsx
@@ -1,4 +1,5 @@
 import { type Match } from "../lib/rating";
+import { DateAndTimePicker } from "./DateAndTimePicker";
 import { UserLookUp } from "./UserLookup";
 
 interface MatchFormProps {
@@ -143,43 +144,13 @@ export const MatchForm = async ({
             Point difference
           </label>
         </div>
+        {/* Only show date and time picker if editing existing match */}
         {match && (
-          <>
-            <div class="group relative mb-6 w-full">
-              <input
-                type="date"
-                value={createdDate}
-                form={formId}
-                name="date_played"
-                id="date_played"
-                class="peer block w-full appearance-none border-0 border-b-2 border-gray-600 bg-transparent px-0 py-2.5 text-sm focus:border-blue-500 focus:outline-none focus:ring-0"
-                placeholder=" "
-              />
-              <label
-                for="date_played"
-                class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-500"
-              >
-                Date played
-              </label>
-            </div>
-            <div class="group relative mb-6 w-full">
-              <input
-                type="time"
-                value={createdTime}
-                form={formId}
-                name="time_played"
-                id="time_played"
-                class="peer block w-full appearance-none border-0 border-b-2 border-gray-600 bg-transparent px-0 py-2.5 text-sm focus:border-blue-500 focus:outline-none focus:ring-0"
-                placeholder=" "
-              />
-              <label
-                for="time_played"
-                class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-500"
-              >
-                Time played
-              </label>
-            </div>
-          </>
+          <DateAndTimePicker
+            formId={formId}
+            initialDate={createdDate}
+            initialTime={createdTime}
+          />
         )}
         {actionButtons}
         <div id="errors" class="text-red-500"></div>

cacb1e0fd382c462cc17a3e570d7005abd43a224
Jonas Nielsen <djonnneller@gmail.com>
2024-10-24 13:46:53 +0200

    Convert createdAt from foreign timezone to utc

--- src/lib/dateUtils.ts
+++ src/lib/dateUtils.ts
@@ -22,3 +22,31 @@ export function getDatePartFromDate(date: Date) {
 export function daysBetween(start: Date, end: Date) {
   return Math.round((end.getTime() - start.getTime()) / dayInMs);
 }
+
+export function fromTimezoneToUTC(date: Date, timezone: string = "Europe/Copenhagen") {
+  const offset = getOffsetMinutes(date, timezone);
+  const dateCopy = new Date(date);
+  dateCopy.setMinutes(dateCopy.getMinutes() - offset);
+  return dateCopy;
+}
+
+function getOffsetMinutes(date: Date, timezone: string) {
+  if (timezone !== "Europe/Copenhagen") throw new Error("Only Europe/Copenhagen is supported for input timezone");
+  const localTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  if (localTimezone !== "UTC") throw new Error("Only UTC is supported for local timezone");
+
+  const lastSundayInMatch = lastSundayOfMonth(date.getFullYear(), 2);
+  const lastSundayInOktober = lastSundayOfMonth(date.getFullYear(), 9);
+  const isSummertime = date >= lastSundayInMatch && date < lastSundayInOktober;
+
+  return isSummertime ? 120 : 60;
+}
+
+function lastSundayOfMonth(year: number, monthIndex: number): Date {
+  const firstOfNextMonth = new Date(year, monthIndex+1, 1);
+  const weekday = firstOfNextMonth.getDay();
+  const dayDiff = weekday===0 ? 7 : weekday;
+  const lastSunday = firstOfNextMonth.setDate(firstOfNextMonth.getDate() - dayDiff);
+
+  return new Date(lastSunday);
+}

ad4e91a8b4de78f039a4316dc4a4b62f3e67aedf
Jonas Nielsen <djonnneller@gmail.com>
2024-11-11 11:09:53 +0100

    Add season selector on leaderboard

--- src/pages/(home).tsx
+++ src/pages/(home).tsx
@@ -1,9 +1,12 @@
 import { Elysia } from "elysia";
 import { ctx } from "../context";
+import { getActiveSeason } from "../db/queries/seasonQueries";
 import { LeaderboardPage } from "./leaderboard";
 
 export const home = new Elysia()
   .use(ctx)
   .get("/", async ({ headers, html, session }) => {
-    return html(() => LeaderboardPage(session, headers));
+    const activeSeason = await getActiveSeason();
+    const activeSeasonId = activeSeason?.id ?? 1;
+    return html(() => LeaderboardPage(session, headers, activeSeasonId));
   });

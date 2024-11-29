e9120e7e641508e2de5b20a9f67b0903cb4c1f04
Simon Bundgaard-Egeberg <simon.bundgaard-egeberg@twoday.com>
2024-11-29 17:01:32 +0100

    feat: a weebhook thing

--- src/controllers/*.ts
+++ src/controllers/*.ts
@@ -1,9 +1,11 @@
 import Elysia from "elysia";
 import { authController } from "./auth";
 import { imageGen } from "./imageGeneration";
+import { webhookController } from "./webhook";
 
 export const api = new Elysia({
   prefix: "/api",
 })
   .use(authController)
+  .use(webhookController)
   .use(imageGen);
---
+++ src/db/schema/webhooks.ts
@@ -0,0 +1,26 @@
+import {
+  integer,
+  sqliteTable,
+  text,
+  uniqueIndex,
+} from "drizzle-orm/sqlite-core";
+
+export const webhookTbl = sqliteTable(
+  "webhooks",
+  {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    url: text("url").notNull(), // The URL where the webhook will be sent
+    secret: text("secret"), // Optional secret for signing webhook payloads
+    eventType: text("event_type").notNull(), // Event type the subscriber is interested in
+    createdAt: integer("created_at").notNull().default(Date.now()), // Creation timestamp
+    updatedAt: integer("updated_at").notNull().default(Date.now()), // Last updated timestamp
+  },
+  (table) => {
+    return {
+      seasonPeriodIdx: uniqueIndex("url").on(table.url),
+    };
+  },
+);
+
+export type Webhook = typeof webhookTbl.$inferSelect;
+export type InsertWebhook = typeof webhookTbl.$inferInsert;

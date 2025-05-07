0c0b9c732f8b1e085dcdadb75083d7fd117bdd8d
Jonas Nielsen <djonnneller@gmail.com>
2025-05-07 22:18:46 +0200

    Add db migration to not use hardcoded timestamp default

---
+++ drizzle/0012_cultured_namor.sql
@@ -0,0 +1,32 @@
+/*
+ SQLite does not support "Drop default from column" out of the box, drizzle does
+ not generate automatic migration for that, so it had to be done manually.
+
+ The below migration statement is implemented following the "Modify column in table"
+ paragraph from https://www.techonthenet.com/sqlite/tables/alter_table.php
+*/
+
+PRAGMA foreign_keys=off;
+
+BEGIN TRANSACTION;
+
+ALTER TABLE webhooks RENAME TO _webhooks_old;
+
+CREATE TABLE webhooks (
+	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	url text NOT NULL,
+	secret text,
+	event_type text NOT NULL,
+	created_at integer NOT NULL,
+	updated_at integer NOT NULL
+);
+
+INSERT INTO webhooks (id, url, secret, event_type, created_at, updated_at)
+  SELECT id, url, secret, event_type, created_at, updated_at
+  FROM _webhooks_old;
+
+DROP TABLE _webhooks_old;
+
+COMMIT;
+
+PRAGMA foreign_keys=on;

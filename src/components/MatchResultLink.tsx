fc6130edb64befc3ba67bbcaeb56b6c567ac917e
Jonas Nielsen <djonnneller@gmail.com>
2025-06-08 13:44:26 +0200

    Update links to result page

--- src/components/MatchResultLink.tsx
+++ src/components/MatchResultLink.tsx
@@ -1,11 +1,10 @@
 interface Props {
   children: JSX.Element;
-  seasonId: number;
   matchId: number;
 }
 
-export const MatchResultLink = ({ children, seasonId, matchId }: Props) => (
-  <a class="underline" href={`/result/${seasonId}/${matchId}`}>
+export const MatchResultLink = ({ children, matchId }: Props) => (
+  <a class="underline" href={`/result/${matchId}`}>
     {children}
   </a>
 );

bc31c29567463baa6233cd8d6ee4f3e5aa0b753a
Jonas Nielsen <djonnneller@gmail.com>
2025-06-08 14:52:13 +0200

    Reimplement eslint

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import eslint from "@eslint/js";
+import prettierDisableOthers from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  prettierRecommended,
+  {
+    plugins: {
+      import: importPlugin,
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      // These off/not-configured-the-way-we-want lint rules we like & opt into
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
+      ],
+      "import/consistent-type-specifier-style": ["error", "prefer-inline"],
+
+      // These lint rules don't make sense for us but are enabled in the preset configs
+      "@typescript-eslint/no-confusing-void-expression": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+
+      // This rule doesn't seem to be working properly
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+    },
+  },
+  prettierDisableOthers,
+]);

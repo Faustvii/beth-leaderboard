71feaea5f54c269fef2044bc630260d68404b21f
Jonas Nielsen <djonnneller@gmail.com>
2025-06-08 13:54:00 +0200

    Add season picker to result page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,12 @@
-export const HeaderHtml = ({ title }: { title: string }) => (
+import { cn } from "../lib/utils";
+
+interface HeaderHtmlProps {
+  title: string;
+  className?: string;
+}
+
+export const HeaderHtml = ({ title, className }: HeaderHtmlProps) => (
   <>
-    <h1 class=" p-5 text-start text-4xl font-bold">{title}</h1>
+    <h1 class={cn("p-5 text-start text-4xl font-bold", className)}>{title}</h1>
   </>
 );

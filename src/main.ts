1f598cdd1aa70ce60aa7448e0962383312dbc7e2
Jonas Nielsen <djonnneller@gmail.com>
2025-11-16 23:29:43 +0100

    Add snow

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "@kitajs/html/register";
 import { migrate } from "drizzle-orm/libsql/migrator";
 import { Elysia } from "elysia";
 import { api } from "./controllers/*";
+import { Christmas, updateIsItChristmas } from "./controllers/christmas";
 import { writeDb } from "./db";
 import { SeedDatabase } from "./db/seed";
 import { pages } from "./pages/*";
@@ -11,10 +12,13 @@ console.log("migrating database");
 await migrate(writeDb, { migrationsFolder: "./drizzle" });
 console.log("database migrated");
 
+updateIsItChristmas();
+
 const app = new Elysia()
   .use(staticController)
   .use(api)
   .use(pages)
+  .use(Christmas)
   .onError(({ error }) => {
     console.error(error);
   })

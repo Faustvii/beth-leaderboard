56d693646701351b0854b4cb4f613522256e30fc
Jonas Nielsen <djonnneller@gmail.com>
2025-11-16 22:38:37 +0100

    Move guest user creation to own page

---
+++ src/pages/admin/guest-user.tsx
@@ -0,0 +1,74 @@
+import { Elysia, t } from "elysia";
+import { type Session } from "lucia";
+import { generateRandomString } from "lucia/utils";
+import { HeaderHtml } from "../../components/header";
+import { LayoutHtml } from "../../components/Layout";
+import { NavbarHtml } from "../../components/Navbar";
+import { ctx } from "../../context";
+import { userTbl, type User } from "../../db/schema/auth";
+import { isHxRequest, redirect } from "../../lib";
+import { syncIfLocal } from "../../lib/dbHelpers";
+import { UserForm } from "./components/UserForm";
+
+export const GuestUser = new Elysia({
+  prefix: "/guest-user",
+})
+  .use(ctx)
+  .get("/", async ({ html, session, headers }) => {
+    return html(() => guestUserPage(session, headers));
+  })
+  .put(
+    "/",
+    async ({ set, headers, body: { name, nickname }, writeDb }) => {
+      const guestName = `${name} (Guest)`;
+      const userToInsert: Omit<User, "picture"> = {
+        id: generateRandomString(15),
+        name: guestName,
+        email: null,
+        roles: null,
+        nickname: nickname,
+      };
+
+      const [{ id: userId }] = await writeDb
+        .insert(userTbl)
+        .values(userToInsert)
+        .returning();
+      await syncIfLocal();
+
+      redirect({ headers, set }, `/profile/${userId}`);
+    },
+    {
+      beforeHandle: (_) => undefined,
+      body: t.Object({
+        name: t.String({ minLength: 1 }),
+        nickname: t.String({ minLength: 1 }),
+      }),
+    },
+  );
+
+export async function guestUserPage(
+  session: Session | null,
+  headers: Record<string, string | null>,
+) {
+  return (
+    <>
+      {isHxRequest(headers) ? (
+        page(session)
+      ) : (
+        <LayoutHtml>{page(session)}</LayoutHtml>
+      )}
+    </>
+  );
+}
+
+async function page(session: Session | null) {
+  return (
+    <>
+      <NavbarHtml session={session} activePage="admin" />
+      <HeaderHtml title="Create guest user" />
+      <div class="flex w-full flex-col gap-3">
+        <UserForm formId="createGuestUser"></UserForm>
+      </div>
+    </>
+  );
+}
